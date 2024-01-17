9b1f4cf5ab753d65952eae9f17db1924fc136708
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-01-17 10:23:47 +0800

    Add a workaround for the AGP lint bug: 'remember calls must not return Unit'

--- sample/src/androidMain/kotlin/com/dokar/sonner/sample/Platform.android.kt
+++ sample/src/androidMain/kotlin/com/dokar/sonner/sample/Platform.android.kt
@@ -1,5 +1,6 @@
 package com.dokar.sonner.sample
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.runtime.Composable
@@ -9,15 +10,17 @@ import androidx.compose.ui.platform.LocalContext
 @Composable
 actual fun rememberLinkOpener(): LinkOpener {
     val context = LocalContext.current
-    return remember(context) {
-        LinkOpener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.setData(Uri.parse(it))
-            try {
-                context.startActivity(intent)
-            } catch (e: Exception) {
-                // Ignore
-            }
+    return remember(context) { AndroidLinkOpener(context) }
+}
+
+private class AndroidLinkOpener(private val context: Context) : LinkOpener {
+    override fun open(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse(url))
+        try {
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            // Ignore
         }
     }
 }
